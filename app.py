af1f007aa0c190da45a2c40513208b948d6724a3
ssampathkumar104 <83206197+ssampathkumar104@users.noreply.github.com>
2026-01-29 07:38:20 +0530

    Simple Chat application added along with streamlit

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+## Langsmith Tracking
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGCHAIN_TRACING"] = "true"
+os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo With Gemma Model")
+input_text=st.text_input("What question you have in mind?")
+
+
+## Ollama Llama2 model
+llm=Ollama(model="gemma3:1b")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
+
